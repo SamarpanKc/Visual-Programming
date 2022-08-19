47c94042e36bfac632cbfa4e55ddbcf37fe38dd8
Samarpan Kc <110466655+SamarpanKc@users.noreply.github.com>
2022-08-19 10:16:14 +0545

    Create Variable.cs

---
+++ C sharp/Variable.cs
@@ -0,0 +1,14 @@
+/*Here i include String and
+integer variables*/
+
+using System;
+namespace Myvariables{
+    class Variables{
+        static void Main(string[] args){
+            string mynameRoll = "Samarpan KC \nRoll.No:";
+            int myInt = 22;
+            string Myclass = "\nClass XII";
+            Console.WriteLine(mynameRoll+myInt+Myclass);
+        }
+    }
+}
