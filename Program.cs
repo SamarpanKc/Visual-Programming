2b5aefb4d4954c9f999f23a9d5dabb1547493728
Samarpan Kc <110466655+SamarpanKc@users.noreply.github.com>
2023-02-10 05:42:52 +0545

    Pointer in C#
    Simple Pointer code<> in C-Sharp Programming Language

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Pointer{
+    public static void Main(){
+        unsafe{
+        int x   = 40;
+        int y   = 9;
+
+        int *p1=&x;
+        int *p2=&y;
+        Console.WriteLine(*p1); // displaying value
+        Console.WriteLine(*p2); // displaying value
+        Console.WriteLine((int)&p1); //displaying address
+        Console.WriteLine((int)&p2); //displaying address
+        }
+   }
+}
