4cba314a386dab8aaa3e7a5b30e78ff2cb791d7c
Samarpan Kc <110466655+SamarpanKc@users.noreply.github.com>
2023-02-12 07:56:45 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Swapping{
+class Pointerswap{
+    static void Main(String []args){
+        int a=10;
+        int b=20;
+        int *p= &a ; 
+        int *q= &b;
+        Console.WriteLine("Before Swap a= {0} , b={1}", a,b);
+        int temp=*p;
+        *p=*q;
+        *q=temp;   
+        Console.WriteLine("After Swap a= {0} , b={1}", a,b);
+
+    }
+}
+}
